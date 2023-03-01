44a2cc69da38324749f3286abad24378a2841934
Manuel Carnero Vergel <73230495+mancarver1@users.noreply.github.com>
2023-03-01 20:27:03 +0100

    Feat: añadidas entidades Peep y Bulletin

---
+++ src/main/java/acme/entities/Bulletin.java
@@ -0,0 +1,52 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Bulletin extends AbstractEntity {
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date				startDate;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			message;
+
+	@NotNull
+	protected Boolean			flag;
+
+
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
