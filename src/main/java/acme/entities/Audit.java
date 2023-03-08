5981c73277e852135e77b3e4f3b1760fb561c571
Manpalpin <manpalpin@alum.us.es>
2023-03-08 23:10:13 +0100

    feat: Entidad Audit
    - Se ha implementado la entidad Audit

---
+++ src/main/java/acme/entities/Audit.java
@@ -0,0 +1,47 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Audit extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			conclusion;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			strongPoints;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			weakPoints;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	//	@ManyToOne
+	//	protected AuditingRecord			auditingRecords;
+}
