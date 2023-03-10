dd10b1ab81386e65135fde4e63202cf08ba7c136
Manpalpin <manpalpin@alum.us.es>
2023-03-10 14:23:39 +0100

    fix: import Auditor

--- src/main/java/acme/entities/Audit.java
+++ src/main/java/acme/entities/Audit.java
@@ -3,12 +3,16 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
 import acme.framework.data.AbstractEntity;
+import acme.roles.Auditor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -46,8 +50,8 @@ public class Audit extends AbstractEntity {
 
 	// Relationships ----------------------------------------------------------
 
-	//  @NotNull
-	//  @Valid
-	//	@ManyToOne(optional = true)
-	//	protected Auditor		auditor;
+	@NotNull
+	@Valid
+	@ManyToOne(optional = true)
+	protected Auditor			auditor;
 }
