f0b8c15e64c515be14d594780ef099680169a971
javiernr02 <javnunrui@alum.us.es>
2023-03-11 22:09:44 +0100

    Tarea 44: Creación entidad Lecture

---
+++ src/main/java/acme/entities/Lecture.java
@@ -0,0 +1,61 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.enums.ContentType;
+import acme.framework.data.AbstractEntity;
+import acme.roles.Lecturer;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Lecture extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			anAbstract;
+
+	@Min(1)
+	protected double			learningTime;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			body;
+
+	@NotNull
+	protected ContentType		contentType;
+
+	@URL
+	protected String			furtherInformation;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Lecturer			lecturer;
+
+}
