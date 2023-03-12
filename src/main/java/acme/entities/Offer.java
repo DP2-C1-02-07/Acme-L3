14a5f5d2cee3ee716b6660321bf5fcee7fb42678
Manpalpin <manpalpin@alum.us.es>
2023-03-12 13:23:51 +0100

    fix: minor fix

--- src/main/java/acme/entities/Offer.java
+++ src/main/java/acme/entities/Offer.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
-import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -60,12 +59,6 @@ public class Offer extends AbstractEntity {
 
 	@URL
 	protected String			link;
-
-
-	@AssertTrue
-	protected boolean isMoneyPositive() {
-		return this.price.getAmount() >= 0.0;
-	}
 
 	// Derived attributes -----------------------------------------------------
 
