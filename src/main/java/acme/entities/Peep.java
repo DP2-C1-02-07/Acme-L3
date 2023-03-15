6a2e4ed4f0141109e1bb57e161d2d6bb139ed152
lintoeli <lintoeli@gmail.com>
2023-03-15 21:08:50 +0100

    Tarea30: Todo funciona, falta añadir más entradas

--- src/main/java/acme/entities/Peep.java
+++ src/main/java/acme/entities/Peep.java
@@ -1,3 +1,4 @@
+
 package acme.entities;
 
 import java.util.Date;
@@ -9,6 +10,7 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -34,12 +36,12 @@ public class Peep extends AbstractEntity {
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	protected Date				startDate;
+	@Past
+	protected Date				instantiationMoment;
 
 	@NotBlank
 	@Length(max = 75)
 	protected String			message;
-	
 
 	@NotBlank
 	@Length(max = 100)
@@ -47,10 +49,9 @@ public class Peep extends AbstractEntity {
 
 	@URL
 	protected String			link;
-	
-	@Email
-	protected String            email;
 
+	@Email
+	protected String			email;
 
 	// Derived attributes -----------------------------------------------------
 
