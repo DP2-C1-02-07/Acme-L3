9edfc94fa4e0a0bba2c1880c633e8ab8bb13ff5d
Manpalpin <manpalpin@alum.us.es>
2023-03-17 10:47:05 +0100

    feat: Sample Data Individual

--- src/main/java/acme/entities/Audit.java
+++ src/main/java/acme/entities/Audit.java
@@ -1,18 +1,12 @@
 
 package acme.entities;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Transient;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -20,7 +14,6 @@ import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
-import acme.entities.enums.Marks;
 import acme.framework.data.AbstractEntity;
 import acme.roles.Auditor;
 import lombok.Getter;
@@ -33,67 +26,56 @@ public class Audit extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long		serialVersionUID	= 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1-3}[0-9]{3}")
-	protected String				code;
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	protected String			code;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				conclusion;
+	protected String			conclusion;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				strongPoints;
+	protected String			strongPoints;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				weakPoints;
-
-	@OneToMany(mappedBy = "audit")
-	private List<AuditingRecords>	auditingRecords;
+	protected String			weakPoints;
 
 	// Derived attributes -----------------------------------------------------
 
 
 	@NotNull
-	@Transient
-	public Marks mark() {
-		final Map<Marks, Integer> marks = new HashMap<>();
-		Marks result;
+	public String mark() {
+		String marks = "N/A";
+		if (!this.auditingRecords.isEmpty())
+			for (int i = 0; i < this.auditingRecords.size(); i++)
+				if (i == 0)
+					marks = this.auditingRecords.get(i).getMark().toString();
+				else
+					marks = marks + ", " + this.auditingRecords.get(i).getMark().toString();
+		return marks;
 
-		for (final AuditingRecords auditingRecord : this.auditingRecords)
-			if (marks.containsKey(auditingRecord.getMark())) {
-				final Integer count = marks.get(auditingRecord.getMark()) + 1;
-				marks.put(auditingRecord.getMark(), count);
-			} else
-				marks.put(auditingRecord.getMark(), 1);
-
-		final int max = Collections.max(marks.values());
-		final List<Marks> moda = new ArrayList<>();
-		for (final Map.Entry<Marks, Integer> entry : marks.entrySet())
-			if (entry.getValue() == max)
-				moda.add(entry.getKey());
-
-		if (moda.size() == 1)
-			result = moda.get(0);
-		else {
-			final Random random = new Random();
-			final int randomInt = random.nextInt(moda.size());
-			result = moda.get(randomInt);
-		}
-		return result;
 	}
 
 	// Relationships ----------------------------------------------------------
 
 
+	@OneToMany(mappedBy = "audit")
+	private List<AuditingRecords>	auditingRecords;
+
 	@NotNull
 	@Valid
-	@ManyToOne(optional = true)
-	protected Auditor auditor;
+	@ManyToOne(optional = false)
+	protected Auditor				auditor;
+
+	//	@NotNull
+	//	@Valid
+	//	@ManyToOne(optional = false)
+	//	protected Course	course;
 }
