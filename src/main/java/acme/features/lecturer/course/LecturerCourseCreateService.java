8a8c10272b6ced253f851620f4c664f98c13a9b8
javiernr02 <javnunrui@alum.us.es>
2023-04-10 09:35:27 +0200

    Tarea 81: Operaciones de lecturers en courses

---
+++ src/main/java/acme/features/lecturer/course/LecturerCourseCreateService.java
@@ -0,0 +1,83 @@
+
+package acme.features.lecturer.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.Course;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+import acme.roles.Lecturer;
+
+@Service
+public class LecturerCourseCreateService extends AbstractService<Lecturer, Course> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected LecturerCourseRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Course object;
+		Lecturer lecturer;
+
+		lecturer = this.repository.findOneLecturerById(super.getRequest().getPrincipal().getActiveRoleId());
+		object = new Course();
+		object.setDraftMode(true);
+		object.setLecturer(lecturer);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Course object) {
+		assert object != null;
+
+		super.bind(object, "code", "title", "anAbstract", "courseType", "retailPrice", "furtherInformation");
+	}
+
+	@Override
+	public void validate(final Course object) {
+		assert object != null;
+
+		if (!super.getBuffer().getErrors().hasErrors("code")) {
+			Course existing;
+
+			existing = this.repository.findOneCourseByCode(object.getCode());
+			super.state(existing == null, "code", "lecturer.course.form.error.duplicated");
+		}
+	}
+
+	@Override
+	public void perform(final Course object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Course object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "code", "title", "anAbstract", "courseType", "retailPrice", "furtherInformation", "draftMode");
+
+		super.getResponse().setData(tuple);
+	}
+
+}
