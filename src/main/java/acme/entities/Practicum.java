c705b16c3bb743671a9a425762ce2528ab9fbe70
lintoeli <lintoeli@gmail.com>
2023-06-30 18:39:02 +0200

    Fix practicum

--- src/main/java/acme/entities/Practicum.java
+++ src/main/java/acme/entities/Practicum.java
@@ -2,6 +2,7 @@
 package acme.entities;
 
 import java.util.Collection;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -73,4 +74,27 @@ public class Practicum extends AbstractEntity {
 	@Valid
 	@ManyToOne(optional = false)
 	protected Course	course;
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(this.abstractThing, this.code, this.company, this.course, this.draftMode, this.goals, this.title);
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		final Practicum other = (Practicum) obj;
+		return Objects.equals(this.abstractThing, other.abstractThing) && Objects.equals(this.code, other.code) && Objects.equals(this.company, other.company) && Objects.equals(this.course, other.course) && this.draftMode == other.draftMode
+			&& Objects.equals(this.goals, other.goals) && Objects.equals(this.title, other.title);
+	}
+
 }
